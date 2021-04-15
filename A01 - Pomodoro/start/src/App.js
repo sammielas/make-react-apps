e347a4c86338309c3020d62918230d2eeabedb3f
Samson Lasode <samflicks@gmail.com>
2021-04-15 14:36:03 +0100

    Changed title, added functionality to start button

--- A01 - Pomodoro/start/src/App.js
+++ A01 - Pomodoro/start/src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+function padTime(time) {
+  return time.toString().padStart(2, '0');
+}
+
 export default function App() {
+  const [timeLeft, setTimeLeft] = useState(0.1 * 60);
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const [title, setTitle] = useState('Let the countdown begin !!!');
+
+  const seconds = padTime(timeLeft - minutes * 60);
+  const startTimer = () => {
+    setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+        return 0;
+      });
+    }, 1000);
+  };
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
