4007e26c56f2a1c18eb1193b80a573829b2ad840
Samson Lasode <samflicks@gmail.com>
2021-04-15 16:37:54 +0100

    added functionaity to stop and reset buttons

--- A01 - Pomodoro/start/src/App.js
+++ A01 - Pomodoro/start/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function padTime(time) {
@@ -9,16 +9,38 @@ export default function App() {
   const [timeLeft, setTimeLeft] = useState(0.1 * 60);
   const minutes = padTime(Math.floor(timeLeft / 60));
   const [title, setTitle] = useState('Let the countdown begin !!!');
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
   const seconds = padTime(timeLeft - minutes * 60);
+
   const startTimer = () => {
-    setInterval(() => {
+    setIsRunning(true);
+    if (intervalRef.current != null) return;
+    setTitle(`Your time starts now!!!`);
+    intervalRef.current = setInterval(() => {
       setTimeLeft((timeLeft) => {
         if (timeLeft >= 1) return timeLeft - 1;
+        resetTimer();
         return 0;
       });
     }, 1000);
   };
+  const stopTimer = () => {
+    setIsRunning(false);
+    if (intervalRef.current === null) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle(`Keep going, don't stop!!!`);
+  };
+  const resetTimer = () => {
+    setIsRunning(false);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle(`Ready to go another round!!!`);
+    setTimeLeft(25 * 60);
+  };
+
   return (
     <div className="app">
       <h2>{title}</h2>
@@ -30,9 +52,9 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
