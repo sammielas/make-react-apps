85ead4f474cb82bd972ab8df82400e4444a04885
Samson Lasode <samflicks@gmail.com>
2021-04-16 13:46:45 +0100

    added markedDown JSX and ReactMarkDown dependency to the project

--- A02 - Markdown Editor/start/src/App.js
+++ A02 - Markdown Editor/start/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 export default function App() {
+  const [markDown, setMarkDown] = useState('# Rendered HTML Here');
+  function markedDown(e) {
+    setMarkDown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea onChange={markedDown} value={markDown} />
 
-      <div className="preview" />
+      {/*  <div className="preview">{markDown}</div> */}
+
+      <ReactMarkdown className="preview" source={markDown} />
     </div>
   );
 }
